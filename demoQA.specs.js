c5f7992c5545d7d61c88b6d743f7a61f64495182
GH012358 <garik012358@gmail.com>
2022-05-02 09:48:30 +0400

    the original commit

---
+++ demoQA.specs.js
@@ -0,0 +1,30 @@
+describe("Form filling", () => {
+
+    it("Open the webpage", () => {
+        let userDetails = {
+            name: "JohnDoe",
+            email: "johndoe@gmail.com",
+            currAddress: "London, UK",
+            permAddress: "Glasgow, UK"
+        };
+
+        Cypress.on('uncaught:exception', (err, runnable) => {
+            // returning false here prevents Cypress from
+            // failing the test
+            return false
+        })
+        cy.visit("https://demoqa.com/");
+        cy.get(".card.mt-4.top-card:first-child").click();
+        cy.get("#item-0").click();
+        cy.get("#userName").clear().type(userDetails.name);
+        cy.get("#userEmail").clear().type(userDetails.email);
+        cy.get("#currentAddress").clear().type(userDetails.currAddress);
+        cy.get("#permanentAddress").clear().type(userDetails.permAddress);
+        cy.get("#submit").click();
+
+        cy.get("p#name").should("have.text", `Name:${userDetails.name}`)
+        cy.get("p#email").should("have.text", `Email:${userDetails.email}`)
+        cy.get("p#currentAddress").should("have.text", `Current Address :${userDetails.currAddress} `)
+        cy.get("p#permanentAddress").should("have.text", `Permananet Address :${userDetails.permAddress}`)
+    });
+});
